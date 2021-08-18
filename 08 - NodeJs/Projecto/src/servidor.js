966d38dd6bd644bd83d42269fa1ef259f4bdd45c
Alexandre Amado <alexandreamado71@gmail.com>
2021-08-18 06:40:19 +0200

    NodeJs

--- 08 - NodeJs/Projecto/src/servidor.js
+++ 08 - NodeJs/Projecto/src/servidor.js
@@ -2,11 +2,25 @@ const porta = 3003
 
 const express = require('express') 
 const app = express() 
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // vai converter para Json
+    res.send(bancoDeDados.getProdutos()) // vai converter para Json
 }) 
+
+app.get('/produtos/id', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name, 
+        preco: req.body.preco
+    }) 
+
+    res.send(produto)
+})
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}`)
-}) 
+})
