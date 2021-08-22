6538cbd5570df7c1e6a68114bd0dbc88ba35b63f
Alexandre Amado <alexandreamado71@gmail.com>
2021-08-22 12:05:30 +0200

    Novos Recursos do Ecma

--- 08 - NodeJs/Projecto/src/servidor.js
+++ 08 - NodeJs/Projecto/src/servidor.js
@@ -3,18 +3,22 @@ const porta = 3003
 const express = require('express') 
 const app = express() 
 const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos()) // vai converter para Json
 }) 
 
-app.get('/produtos/id', (req, res, next) => {
+app.get('/produtos/:id', (req, res, next) => {
     res.send(bancoDeDados.getProdutos(req.params.id))
 })
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name, 
+        nome: req.body.nome, 
         preco: req.body.preco
     }) 
 
--- 09 - ESNext/promese_1.js
+++
@@ -1,6 +0,0 @@
-let p = new Promise(function (cumprirPromesa) {
-    cumprirPromesa(['Ana', 'Bia', 'Carlos', 'Daniel'])
-}) 
-
-.then(valor => valor[0])
-.then(valor => console.log(valor)) 
